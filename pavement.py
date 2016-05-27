8e16597a3cfa9941240e49a1e4f10a4be8c43a3e
hufeiya <a4188@qq.com>
2016-05-27 22:23:44 +0800

    update all files

---
+++ pavement.py
@@ -0,0 +1,37 @@
+import paver
+from paver.easy import *
+import paver.setuputils
+paver.setuputils.install_distutils_tasks()
+
+
+######## CHANGE THIS ##########
+project_name = "pythonds"
+###############################
+
+master_url = 'http://127.0.0.1:8000'
+master_app = 'runestone'
+serving_dir = './build/pythonds'
+dest = '../../static'
+
+options(
+    sphinx = Bunch(docroot=".",),
+
+    build = Bunch(
+        builddir="./build/"+project_name,
+        sourcedir="./_sources/",
+        outdir="./build/"+project_name,
+        confdir=".",
+        project_name = project_name,
+        template_args = {
+            'course_id':project_name,
+            'login_required':'false',
+            'appname':master_app,
+            'loglevel':10,
+            'course_url':master_url,
+            'use_services': 'true',
+            'python3': 'true',
+        }
+    )
+)
+
+from runestone import build  # build is called implicitly by the paver driver.
